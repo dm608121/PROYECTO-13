84f105f6e6a0fe4ac21c06cb8ec085af6358ee1f
dm608121 <112925396+dm608121@users.noreply.github.com>
2022-09-16 17:30:46 -0500

    Add files via upload

---
+++ Alimenta-al-conejo-plantilla--main/sketch.js
@@ -0,0 +1,141 @@
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
+}
+
+
+function setup(){
+  
+  createCanvas(400,400);
+//Mover fondo
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+
+//crear sprite rabbit 
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+
+var rand = Math.round(random(1,100))
+console.log(rand)
+
+ 
+
+
+
+}
+
+function draw() {
+  background(0);
+
+
+  
+  
+  //mover el sprite rabbit en el eje X con el mouse
+  rabbit.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  
+  createRed()
+  createOrange()
+  createApples()
+   
+  
+   drawSprites();
+   
+  
+// var select_sprites = Math(random(1,3));
+
+// var select_sprites = Math.random(random(1,3));
+
+// var select_sprites = Math.round(1,3);
+
+// var select_sprites = Math.round(random(1,3));
+
+
+  
+      
+    }
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+  // if (frameCount % 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }
+  // }
+
+  // if (frameCount / 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+  // if (frameCount % 80 = 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+
+
+
+
+
+function createApples() {
+if(frameCount %80===0){
+ 
+apple = createSprite(random(50, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY = 3;
+apple.lifetime = 150;
+}
+}
+
+function createOrange() {
+  if(frameCount %80===0){
+orangeL = createSprite(random(50, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 150;
+  }
+}
+
+function createRed() {
+  if(frameCount %80===0){
+redL = createSprite(random(50, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
+  }
+  }
+
